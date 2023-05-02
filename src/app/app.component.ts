61ff19252d3280722207397e16443582e371ecc2
Baruch Giverts <bor770@gmail.com>
2023-05-02 17:02:48 +0300

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+  imports: [CommonModule],
   selector: 'app-root',
+  standalone: true,
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'huddle-landing-page-with-a-single-introductory-section';
