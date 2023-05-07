98a5852cb848fb636691a2199ef09bc8d2048f8d
Baruch Giverts <bor770@gmail.com>
2023-05-07 14:52:12 +0300

    finished html

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,20 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatIconModule } from '@angular/material/icon';
-// import {
-//   faInstagram,
-//   faFacebook,
-//   faTwitter,
-// } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  imports: [CommonModule,  MatIconModule],
+  imports: [CommonModule, MatIconModule],
   selector: 'app-footer',
   standalone: true,
   styleUrls: ['./footer.component.css'],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-  // icons = [faFacebook, faTwitter, faInstagram];
+  socialIcons = [`facebook`, `twitter`, `instagram`];
 }
